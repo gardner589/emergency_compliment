a2cfb4b3d03e3c004d98df558bcf2ec6c29c11a2
Julia Schultz <julia.schultz91@gmail.com>
2016-07-15 09:44:59 -0400

    added un-insults controller

---
+++ js/un-insults/uninsults.controller.js
@@ -0,0 +1,11 @@
+`use strict`;
+
+(function(){
+  var app = angular.module("uninsults");
+  app.controller("uninsultsController", uninsultsController);
+
+  function uninsultsController(){
+    var vm = this;
+  }
+
+}());
